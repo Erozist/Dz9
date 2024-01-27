9e973622fefdf53ef6772af264e4dbfa61359976
Erozist <Erozist87@gmail.com>
2024-01-27 22:59:29 +0200

    Commit 4

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def input_error(func):
         try:
             return func(*args)
         except (IndexError, KeyError, ValueError):
-            pass
+            return 'Name or Number not correct!'
     return inner
 
 
@@ -27,14 +27,16 @@ def handler(comand_list):
     ''' Функція обробки введених данних '''
     comand = comand_list.lower().split()
     if comand_list == '':
-        return lambda *_: 'Correct the input!!! The entered command is not valid!!!'
+        return lambda *_: 'Correct the input!!! The entered command is empty!!!'
+    if len(comand) > 3:
+        return lambda *_: 'You comand is very long'
     if len(comand) == 2:
         if comand[0] == 'good' and comand[1] == 'bye' or\
            comand[0] == 'show' and comand[1] == 'all':
             return OPERATIONS[comand_list]
     if comand[0] in OPERATIONS:
         return OPERATIONS[comand[0]]
-    return lambda *_: 'Correct you input!'
+    return lambda *_: f'Correct the input!!! The entered command <{comand}> is not valid!!!'
 
 @input_error
 def handler_hello(*_):
@@ -42,22 +44,22 @@ def handler_hello(*_):
 
 @input_error
 def handler_add(comand, phone_book):
+    print(comand[1], comand[2])
     if comand[1] in phone_book:
-        phone_book[comand[1]] = phone_book.get(comand[1], []) + [comand[2]]
-        return f'A new entry {comand[2]} is added to the contact {comand[1]}.'
-    phone_book.update({comand[1]: [comand[2]]})
+        return f'<{comand[1]}> is already a contact.'
+    phone_book.update({comand[1]: comand[2]})
     return f'A new contact {comand[1]} has been created.'
 
 @input_error
 def handler_change(comand, phone_book):
-    if [comand[1]] in phone_book:
+    if comand[1] in phone_book:
         phone_book[comand[1]] = [comand[2]]
         return f'A new number has been recorded {comand[2]}.'
     return f"The contact {[comand[1]]} is not in the phone book."
 
 @input_error
 def handler_phone(comand, phone_book):
-    if [comand[1]] in phone_book:
+    if comand[1] in phone_book:
         return f"The contact {[comand[1]]} phone number {phone_book[comand[1]]}."
     return f"The contact {[comand[1]]} is not in the phone book."
 
